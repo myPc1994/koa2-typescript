c88bfdeba0741e5d33846bbfded546bc4abeda07
CPC1994 <939979509@qq.com>
2021-09-23 22:15:24 +0800

    feat

--- env/production.js
+++ env/production.js
@@ -10,14 +10,14 @@ module.exports = {
     type: "file",//file,db//文件保存，还是数据库保存
   },
   mongodb_config: { //mongodb数据库配置
-    user: "root",
-    pass: "123456",
-    host: "cpc_db",// mongodb容器的别名
-    port: 27017,
-    database: "test",
-    option: {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }
+      user: "",
+      pass: "",
+      host: "127.0.0.1",
+      port: 27017,
+      database: "mobileApp",
+      option: {
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+      }
   },
 };
